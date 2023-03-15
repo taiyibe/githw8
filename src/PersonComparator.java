22b92b2026f90379a16bd9974d209fa15269efa2
Viktoria Kravchenko <taiyibe@yandex.ru>
2023-03-15 16:36:23 +0300

    Вторая версия программы

--- src/PersonComparator.java
+++ src/PersonComparator.java
@@ -1,14 +1,30 @@
 import java.util.Comparator;
 
 public class PersonComparator implements Comparator<Person> {
+    private int MaxWords;
+
+    public PersonComparator(int maxWrd) {
+        MaxWords = maxWrd;
+    }
+
     @Override
     public int compare(Person o1, Person o2) {
         int w1 = o1.getSurname().split(" ").length;
         int w2 = o2.getSurname().split(" ").length;
-        if (w1 < w2) {
-            return 1;
-        } else if (w1 > w2) {
-            return -1;
+        if (w1 < MaxWords || w2 < MaxWords) {
+            if (w1 < w2) {
+                return 1;
+            } else if (w1 > w2) {
+                return -1;
+            } else {
+                if (o1.getAge() < o2.getAge()) {
+                    return 1;
+                } else if (o1.getAge() > o2.getAge()) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
         } else {
             if (o1.getAge() < o2.getAge()) {
                 return 1;
@@ -19,5 +35,4 @@ public class PersonComparator implements Comparator<Person> {
             }
         }
     }
-
 }
